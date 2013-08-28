847ff32b339bc49e7b4c723e545a65cc46fda2b1
Zander <zanderfick1@hotmail.com>
2013-08-28 08:25:13 +0200

    Yields a graph but is completely wrong

--- VLE.py
+++ VLE.py
@@ -5,10 +5,11 @@ import Psat
 R = 8.314472
 
 
-def G_RT_main_l(T, P_bar, Vmix, x1,Tc_1, Pc_1_bar, Tc_2, Pc_2_bar, m_1, m_2):
+def G_RT_main(T, P_bar, x1,Tc_1, Pc_1_bar, Tc_2, Pc_2_bar, m_1, m_2):
     Pc_1 = Pc_1_bar*100
     Pc_2 = Pc_2_bar*100
     P = P_bar*100    
+    x2 = 1-x1
 
     a_1 = Psat.a(T, Tc_1, Pc_1, m_1)
     a_2 = Psat.a(T, Tc_2, Pc_2, m_2)
@@ -16,20 +17,43 @@ def G_RT_main_l(T, P_bar, Vmix, x1,Tc_1, Pc_1_bar, Tc_2, Pc_2_bar, m_1, m_2):
     b_1 = (R*Tc_1)/(8*Pc_1)
     b_2 = (R*Tc_2)/(8*Pc_2)    
     
-    
     amix = a_mix(a_1, a_2, x1)
     bmix = b_mix(b_1, b_2, x1)
     
-    volumes_l = dV_l(R, P_bar, x1, Tc_1, Pc_1_bar, Tc_2, Pc_2_bar, m_1, m_2)
+    volumes = dV(T, P_bar, x1, Tc_1, Pc_1_bar, Tc_2, Pc_2_bar, m_1, m_2)
+   
+    delta_V = volumes[0]
+    V_i = [volumes[1], volumes[2]]
+    Vmix = volumes[3]
     
-    Term_1 = 
+    term_1 = P*delta_V[0]/R*T
+
+    term_2_1 = numpy.log((V_i[0]-b_1)/(Vmix-bmix))
+    term_2_2 = numpy.log((V_i[1]-b_2)/(Vmix-bmix))
+    term_2 = x1*term_2_1 + x2*term_2_2
+    print V_i
+    term_3_1 = a_1/(R*T*V_i[0])
+    term_3_2 = a_2/(R*T*V_i[1])
     
-def dV_l(T, P_bar, x1, Tc_1, Pc_1_bar, Tc_2, Pc_2_bar, m_1, m_2, Vmixture):
-    single_1 = V_single(T, Tc, P_bar, Pc_1_bar, m_1)[0]
-    single_2 = V_single(T, Tc, P_bar, Pc_2_bar, m_2)[0]
+    term_3 = x1*term_3_1 + x2*term_3_2
+    
+    term_4 = -amix/(R*T*Vmix)
+    
+    terms = [term_1, term_2, term_3, term_4]
+    return sum(terms)
+
+    
+def dV(T, P_bar, x1, Tc_1, Pc_1_bar, Tc_2, Pc_2_bar, m_1, m_2):
+    single_1 = V_single(T, Tc_1, P_bar, Pc_1_bar, m_1)[0]
+    single_2 = V_single(T, Tc_2, P_bar, Pc_2_bar, m_2)[0]
+    Vmixture =  V_mixture(x1, single_1, single_2)
     x2 = 1 - x1
-    sigma = single_1*x1 + single2*x2
-    return [Vmixture - sigma, single_1, single_2]
+    sigma = single_1*x1 + single_2*x2
+    return [Vmixture - sigma, single_1, single_2, Vmixture]
+
+def V_mixture(x1, V_1, V_2):
+    x2 = 1-x1
+    return x1*V_1 + x2*V_2
 
 
 def V_single(T, Tc, P_bar, Pc_bar, m):
@@ -47,10 +71,10 @@ def V_single(T, Tc, P_bar, Pc_bar, m):
     
     if P > sat_P:
         V_id_l = sc_o.fsolve(eq, [0.99*calc[1]])
-        return [V_id_l, 0]
+        return [V_id_l]
     elif P < sat_P:
         V_id_v = sc_o.fsolve(eq, [1.01*calc[2]])
-        return [0, V_id_v]
+        return [V_id_v]
     else:
         V_id_l = calc[1]
         V_id_v = calc[2]
@@ -60,7 +84,7 @@ def x2(x1):
     return 1 - x1
 
 def a_mix(a11, a22, x1):
-    a12 = (a11 +a22)/2
+    a12 = numpy.sqrt(a11*a22)
     x2 = 1 - x1
     return a11*x1**2 + 2*a12*x1*x2 + a22*x2**2
 
@@ -68,4 +92,15 @@ def b_mix(b1, b2, x1):
     x2 = 1 - x1
     return b1*x1 + b2*x2
 
-print V_single(500, 508.1, 50, 47.02, 0.975)[0]
+#           G_RT_main(T, P_bar, x1,Tc_1, Pc_1_bar, Tc_2, Pc_2_bar, m_1, m_2)
+
+
+import pylab
+Data_x = numpy.linspace(0,1)
+Data_y_1 = G_RT_main(500, 30, Data_x, 508.1, 47.02, 647.3, 220.64, 0.967, 1.014)
+Data_y_2=  G_RT_main(450, 30, Data_x, 508.1, 47.02, 647.3, 220.64, 0.967, 1.014)
+pylab.plot(Data_x, Data_y_1, 'r')
+pylab.plot(Data_x, Data_y_2, 'b')
+pylab.show()
+
+
